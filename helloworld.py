e4506976ac32925a948beb3060374e58c4051708
Ravi Shah <RShah@FusionStorm.com>
2020-03-03 16:32:27 -0500

    changes to helloworld script

--- helloworld.py
+++ helloworld.py
@@ -3,15 +3,15 @@ import socket
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def index():
-    #try:
-        #host_name = socket.gethostname()
-        #host_ip = socket.gethostbyname(host_name)
-    return render_template('index.html')
-    #except:
-    #    return render_template('error.html')
+    try:
+        host_name = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+        return render_template('index.html', hostname=host_name, ip=host_ip)
+    except:
+        return render_template('error.html')
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
